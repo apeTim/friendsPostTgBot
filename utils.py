9429373f4232b70f42ebcc1ed7550a8dae37d392
Tim <apetimx@gmail.com>
2023-01-23 19:13:06 +0300

    fix sanitise_text

--- utils.py
+++ utils.py
@@ -1,23 +1,26 @@
 LINE_ENDS = ['.', '!', ',', '?', ':', ';']
 
 def sanitise_text(text: str) -> str:
-        sanitised_text = ''
+    if not text:
+        return None
+    
+    sanitised_text = ''
 
-        for char in text:
-            if not (ord(char) == 10 or 31 < ord(char) < 127 or 1039 < ord(char) < 1104): continue
-            if char == '\n' and len(sanitised_text) == 0: continue
+    for char in text:
+        if not (ord(char) == 10 or 31 < ord(char) < 127 or 1039 < ord(char) < 1104): continue
+        if char == '\n' and len(sanitised_text) == 0: continue
 
-            if char == '\n':
-                if sanitised_text[-1] == ' ':
-                    sanitised_text = sanitised_text[:-1]
-                if sanitised_text[-1] in LINE_ENDS:
-                    sanitised_text += ' '
-                    continue
+        if char == '\n':
+            if sanitised_text[-1] == ' ':
+                sanitised_text = sanitised_text[:-1]
+            if sanitised_text[-1] in LINE_ENDS:
+                sanitised_text += ' '
+                continue
 
-                sanitised_text += '. '
-            else:
-                sanitised_text += char
-        
-        sanitised_text = sanitised_text.strip()
+            sanitised_text += '. '
+        else:
+            sanitised_text += char
+    
+    sanitised_text = sanitised_text.strip()
 
-        return sanitised_text
+    return sanitised_text
